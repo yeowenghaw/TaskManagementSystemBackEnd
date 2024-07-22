1b2feb3ee66c1a2f79879d3fcf3565650158cd24
Yeow Eng Haw <enghaw.yeow@stengg.com>
2024-07-22 08:50:46 +0800

    fixed previous startup error, was calling getSample in sample.js but exporting getSamples in samplecontroller.js, server can start up and can make a call through localhost successfully

--- controller/sampleController.js
+++ controller/sampleController.js
@@ -1,9 +1,7 @@
-
-
-exports.getSamples = (req, res, next) => {
+// /api/v1/sample
+exports.getSample = (req, res, next) => {
   res.status(200).json({
-    success : true,
-    message : 'this is a sample message'
-  }
-  );
-}
+    success: true,
+    message: "this is a sample message"
+  });
+};
--- routes/sample.js
+++ routes/sample.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+// importing controller methods form sampleCotroller
 const { getSample } = require("../controller/sampleController");
 
 router.route('/sample').get(getSample);
